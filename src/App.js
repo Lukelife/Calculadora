cccfd5ce8e9d2bce1ab1a760629419f21a6e41ed
Lukelife <lcsguedesf@gmail.com>
2022-12-10 14:01:19 -0300

    subindo alterações e funcionalidades

--- src/App.js
+++ src/App.js
@@ -21,6 +21,68 @@ const App = () => {
     setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
   }
 
+
+  const handlesqrNumbers = () => {
+
+      const sqc = Math.sqrt(currentNumber);
+      setCurrentNumber(String(sqc))
+      setOperation('')
+
+  }
+
+
+
+  const handlepercentNumbers = () => {
+
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('%')
+    }else {
+      const perc = (Number(firstNumber) * Number(currentNumber)) / 100;
+      setCurrentNumber(String(perc))
+      setOperation('')
+    }
+
+  }
+
+
+  const handlenegatNumbers = () => {
+
+      const negat =  Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(negat))
+      setOperation('')
+  }
+
+  const handledivNumbers = () => {
+
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('/')
+    }else {
+      const div = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(div))
+      setOperation('')
+    }
+
+  }
+
+
+  const handleMultNumbers = () => {
+
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('x')
+    }else {
+      const mult = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(mult))
+      setOperation('')
+    }
+
+  }
+
   const handleSumNumbers = () => {
 
     if(firstNumber === '0'){
@@ -34,6 +96,7 @@ const App = () => {
     }
 
   }
+ 
 
   const handleMinusNumbers = () => {
 
@@ -42,8 +105,8 @@ const App = () => {
         setCurrentNumber('0')
         setOperation('-')
     }else {
-      const sum = Number(firstNumber) - Number(currentNumber);
-      setCurrentNumber(String(sum))
+      const min = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(min))
       setOperation('')
     }
 
@@ -59,8 +122,14 @@ const App = () => {
           case '-':
             handleMinusNumbers();
             break;
-          default: 
+          case 'x':
+            handleMultNumbers();
             break;
+          case '/':
+            handledivNumbers();
+            break;  
+          default: 
+            
         }
     }
 
@@ -71,28 +140,36 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x"/>
-          <Button label="/"/>
+        <Button label="√" onClick={() => handlesqrNumbers('√')}/>
+          <Button label="%" onClick={() => handlepercentNumbers('%')}/>
           <Button label="c" onClick={handleOnClear}/>
-          <Button label="."/>
+          <Button label="÷" onClick={handledivNumbers}/>
+
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
+          <Button label="x" onClick={() =>handleMultNumbers('x')}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="-" onClick={handleMinusNumbers}/>
+          
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
+          <Button label="+" onClick={handleSumNumbers}/>
+        </Row>
+        <Row>
+        <Button label="±" onClick={() => handlenegatNumbers('±')}/>
+        <Button label="0" onClick={() => handleAddNumber('0')}/>
+        <Button label="."onClick={() => handleAddNumber('.')}/>
+        <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
     </Container>
